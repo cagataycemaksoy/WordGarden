91045556fcceb0e6475b25bc95343ea80775b849
cagataycemaksoy <cemaksoy7890@gmail.com>
2025-11-09 21:01:28 +0100

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Cem Aksoy on 9.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
